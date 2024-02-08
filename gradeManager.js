c97b87bcfb5768de3a6ce68084f20552de89789b
sebastianlopezanido <lopezanidosebastian@gmail.com>
2024-02-08 08:51:11 -0300

    update final submission

--- gradeManager.js
+++ gradeManager.js
@@ -1,66 +1,78 @@
-const jsonist = require('jsonist')
+const axios = require('axios')
+const { parser } = require('stream-json')
+const { chain } = require('stream-chain')
+const {streamArray} = require('stream-json/streamers/StreamArray')
 
 class GradeManager {
-    static instance
-    grades = []
-    constructor() {
-        if (!GradeManager.instance) {
-            GradeManager.instance = this
-        }
-        return GradeManager.instance;
+  static instance
+  grades = []
+  courseStats = {}
+  constructor() {
+    if (!GradeManager.instance) {
+      GradeManager.instance = this
     }
+    return GradeManager.instance
+  }
 
-    async update() {
-        try {
-            const url = 'https://outlier-coding-test-data.onrender.com/grades.json'
-            const { data, response } = await jsonist.get(url)
-            if (response.statusCode !== 200) {
-                throw new Error('Error connecting to grades.json')
+  async update() {
+    try {
+      const grades = []
+      const courseStats = {}
+      const response = await axios({
+        method: "get",
+        url: "https://outlier-coding-test-data.onrender.com/grades.json",
+        responseType: "stream",
+      })
+      const pipeline = chain([response.data, parser(), streamArray()])
+      return new Promise((resolve, reject) => {
+        pipeline.on("data", (data) => {
+          grades.push(data.value)
+          const courseId = data.value.course
+          const gradeValue = data.value.grade
+          if (!courseStats[courseId]) {
+            courseStats[courseId] = {
+              highestGrade: gradeValue,
+              lowestGrade: gradeValue,
+              totalGrade: gradeValue,
+              studentCount: 1
             }
-            this.grades = data
-        } catch (error) {
-            console.error('Error fetching data')
-        }
-    }
-
-    getGradeStudent(studentId) {
-        const filteredGrades = this.grades.filter(grade => grade.id === parseInt(studentId)).map(({ course, grade }) => ({ course, grade }))
-        return filteredGrades
-    }
-
-    getCourseStats() {
-        const courseStats = {}
-
-        this.grades.forEach(grade => {
-            const courseId = grade.course
-            const gradeValue = grade.grade
-
-            if (!courseStats[courseId]) {
-                courseStats[courseId] = {
-                    highestGrade: gradeValue,
-                    lowestGrade: gradeValue,
-                    totalGrade: gradeValue,
-                    studentCount: 1
-                }
-            } else {
-                courseStats[courseId].highestGrade = Math.max(courseStats[courseId].highestGrade, gradeValue)
-                courseStats[courseId].lowestGrade = Math.min(courseStats[courseId].lowestGrade, gradeValue)
-                courseStats[courseId].totalGrade += gradeValue
-                courseStats[courseId].studentCount++
-            }
+          } else {
+            courseStats[courseId].highestGrade = Math.max(
+              courseStats[courseId].highestGrade,
+              gradeValue
+            )
+            courseStats[courseId].lowestGrade = Math.min(
+              courseStats[courseId].lowestGrade,
+              gradeValue
+            )
+            courseStats[courseId].totalGrade += gradeValue
+            courseStats[courseId].studentCount++
+          }
         })
-
-        Object.keys(courseStats).forEach(courseId => {
-            const stats = courseStats[courseId]
-            stats.averageGrade = (stats.totalGrade / stats.studentCount).toFixed(2)
-            delete stats.totalGrade
-            delete stats.studentCount
+        pipeline.on("end", (...args) => {
+          console.log("end updating grade", args)
+          this.grades = grades
+          this.courseStats = courseStats
+          resolve(args)
         })
-
-        return courseStats
+        pipeline.on("error", reject)
+      })   
+    } catch (error) {
+      console.error("Error downloading the JSON file:", error)
     }
+  }
+
+  getGradeStudent(studentId) {
+    const filteredGrades = this.grades.filter(grade => grade.id === parseInt(studentId)).map(({ course, grade }) => ({ course, grade }))
+    return filteredGrades
+}
+
+  getCourseStats() {
+    const courseStats = this.courseStats
+    return courseStats
+  }
 }
 
 module.exports = {
-    GradeManager
+  GradeManager,
 }
